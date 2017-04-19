1918b9669b7590f5b3b0037d5a3059923093c40b
adam <adamgajek93@gmail.com>
2017-04-19 22:27:24 +0200

    Added timestamps about start and end time of lambda

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const AWS = require('aws-sdk');
 const exec = require('child_process').exec;
 
 exports.handler = (event, context) => {
+  const start = Date.now();
+
   exec("./runme64", (error, stdout, stderr) => {
-    let s3 = new AWS.S3({ signatureVersion: 'v4' });
+    const s3 = new AWS.S3({ signatureVersion: 'v4' });
+
+    const end = Date.now();
+    const diff = end - start
+    const output = stdout + start + " " + end + " " + diff.toString();
 
     s3.putObject({
       Bucket: 'agajek-lambda',
-      Key:  event.dir + "/linpack",
-      Body: stdout,
+      Key: event.dir + "/linpack",
+      Body: output,
       ACL: 'public-read'
     }, s3Callback);
-
   });
 
   function s3Callback(err, resp) {
